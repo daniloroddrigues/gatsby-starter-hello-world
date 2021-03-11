d52a8f575774487f994d9a435408791735b74204
Danilo Silva Rorigues <danilo.roddrigues@gmail.com>
2021-03-11 14:24:35 -0300

    criando paginas atravez dos fields slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,51 @@
-exports.onCreateNode = ({ node }) => {
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
   if (node.internal.type === `MarkdownRemark`) {
+    // verifica o tipo se é MarkdownRemark
     console.log(node.internal.type)
+
+    // função getNode obtem um node por um ID
+    // const fileNode = getNode(node.parent)
+    // console.log(`\n`, fileNode.relativePath)
+
+    // cria páginas a partir dos markdowns criados na pasta pages
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(JSON.stringify(result, null, 4))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
