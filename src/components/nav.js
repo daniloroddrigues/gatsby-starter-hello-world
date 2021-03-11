26f7c640cc567bbc78b37e6fdb891199de4e8f91
Danilo Silva Rorigues <danilo.roddrigues@gmail.com>
2021-03-11 13:51:48 -0300

    component navigation header

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,42 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import * as navStyles from "./nav.module.css"
 
+const LinkList = props => (
+  <li>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
 export default function Nav() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
   return (
-    <nav className={navStyles.navgation}>
-      <ul>
-        <li>
-          <Link to="/contact/">Contact</Link>
-        </li>
-        <li>
-          <Link to="/about/">About</Link>
-        </li>
-        <li>
-          <Link to="/about-css-modules/">About Css Modules</Link>
-        </li>
-      </ul>
-    </nav>
+    <div className={navStyles.navgation}>
+      <header>
+        <Link to="/">
+          <h3>{data.site.siteMetadata.title}</h3>
+        </Link>
+        <ul>
+          <LinkList to="/">Home</LinkList>
+          <LinkList to="/about/">About</LinkList>
+          <LinkList to="/pandas/">Pandas</LinkList>
+          <LinkList to="/my-files/">My Files</LinkList>
+          <LinkList to="/about-css-modules/">CSS Modules</LinkList>
+          <LinkList to="/mark/">Markdown Remark</LinkList>
+          <LinkList to="/contact/">Contact</LinkList>
+        </ul>
+      </header>
+    </div>
   )
 }
